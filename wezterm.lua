5d48b3ac59aa9fb9d9cbd83f175241a5e6ae9e01
huggo-42 <victorhuggocontato42@gmail.com>
2024-02-05 01:48:57 -0300

    feat: add wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -26,25 +26,24 @@ config.font_size = 16.0
 -- for text, so we're going to dim it down to 10% of its normal brightness
 local dimmer = { brightness = 0.1 }
 
-local file_path = 'path_to_background_image'
-
-config.background = {
-  -- This is the deepest/back-most layer. It will be rendered first
-  {
-    source = {
-      File = file_path,
-    },
-    -- The texture tiles vertically but not horizontally.
-    -- When we repeat it, mirror it so that it appears "more seamless".
-    -- An alternative to this is to set `width = "100%"` and have
-    -- it stretch across the display
-    repeat_x = 'Mirror',
-    hsb = dimmer,
-    -- When the viewport scrolls, move this layer 10% of the number of
-    -- pixels moved by the main viewport. This makes it appear to be
-    -- further behind the text.
-    attachment = { Parallax = 0.1 },
-  },
-}
+-- local file_path = 'path to background image'
+-- config.background = {
+--   -- This is the deepest/back-most layer. It will be rendered first
+--   {
+--     source = {
+--       File = file_path,
+--     },
+--     -- The texture tiles vertically but not horizontally.
+--     -- When we repeat it, mirror it so that it appears "more seamless".
+--     -- An alternative to this is to set `width = "100%"` and have
+--     -- it stretch across the display
+--     repeat_x = 'Mirror',
+--     hsb = dimmer,
+--     -- When the viewport scrolls, move this layer 10% of the number of
+--     -- pixels moved by the main viewport. This makes it appear to be
+--     -- further behind the text.
+--     attachment = { Parallax = 0.1 },
+--   },
+-- }
 
 return config
